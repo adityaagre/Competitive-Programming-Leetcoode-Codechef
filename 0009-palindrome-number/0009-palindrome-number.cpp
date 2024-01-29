e774b9f3d88c40f2f531a97cb687754675a1021d
Aditya Agre <adityaagrecollege@gmail.com>
2024-01-29 23:04:28 +0530

    Time: 12 ms (35.74%), Space: 8.1 MB (15.90%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    bool isPalindrome(int x) 
+    {
+        if(x==0)
+        {
+            return true;
         }
-
-        long long reversed = 0;
-        long long temp = x;
-
-        while (temp != 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
+        
+        else
+        {
+            long long seedha = x;
+            long long ulta = 0;
+            
+            if(x>0)
+            {
+                while(x>0)
+                {
+                    int last_dig = x%10;
+                    x = x/10;
+                    
+                    ulta = ulta*10 + last_dig;
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            
+            return(ulta == seedha);
         }
-
-        return (reversed == x);
+        
+        
     }
 };
