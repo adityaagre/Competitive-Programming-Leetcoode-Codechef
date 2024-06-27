1854c2775483e52595930d0820e4e87a1d890294
Aditya Agre <adityaagrecollege@gmail.com>
2024-06-27 14:24:28 +0530

    Time: 128 ms (82.10%), Space: 70.9 MB (54.65%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -4,20 +4,20 @@ public:
     {
         // int n = edges.size();
         // vector<int> e1 = edges[0];
-        // vector<int> e2 = edges[1];
+        vector<int> e2 = edges[1];
         
         // int a = e1[0];
         // int b = e1[1];
         // int c = e2[0];
         // int d = e2[1];
         
-        if(count(edges[0].begin(), edges[0].end(), edges[1][0]))
+        if(count(edges[0].begin(), edges[0].end(), e2[0]))
         {
-            return edges[1][0];
+            return e2[0];
         }
         else
         {
-            return edges[1][1];
+            return e2[1];
         }
         return -1;
     }
